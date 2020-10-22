7c2a76909b222f7822883c12736ce90b388d07c5
osipenkoDenis <denis.osipenko@inbox.ru>
2020-10-22 23:16:35 +0300

    complete the task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,34 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let res = '';
+    let tmpArr = expr.split('*'.repeat(10));
+    let morseWordArray = [];
+    for (let i = 0; i < tmpArr.length; i++) {
+        for (let j = 9; j < tmpArr[i].length; j += 10) {
+            morseWordArray.push(parseInt(tmpArr[i].substring(j + 1 - 10, j + 1)).toString());
+        }
+        morseWordArray.push(' ');
+    }
+    let classicWord = '';
+    for (let k = 0; k < morseWordArray.length; ++k) {
+        if (morseWordArray[k] === ' ') {
+            res += ' ';
+        } else {
+            for (let p = 0; p < morseWordArray[k].length; p += 2) {
+                if (morseWordArray[k][p] + morseWordArray[k][p + 1] === '11') {
+                    classicWord += '-'
+                } else if (morseWordArray[k][p] + morseWordArray[k][p + 1] === '10') {
+                    classicWord += '.';
+                }
+            }
+            res += MORSE_TABLE[classicWord];
+            classicWord = '';
+        }
+    }
+
+
+    return res.slice(0, -1);
 }
 
 module.exports = {
